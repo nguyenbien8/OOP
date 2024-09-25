5e6d1c35a76320bcda8714617a39065c6f23e198
nguyenbien8 <nguyenbien5102005@gmail.com>
2024-09-25 09:07:01 +0700

    update_homework

---
+++ HW1/Introduction.java
@@ -0,0 +1,22 @@
+
+public class Introduction {
+    public static void main(String[] args) {
+        String ten = "Nguyen Van Bien";
+        int MSSV = 23021477;
+        String lop = "K68I-CS3";
+        String username_github = "nguyenbien8";
+        String email = "nguyenbien5102005@gmail.com";
+        System.out.println(ten + "\t" + MSSV + "\t" + lop + "\t" + username_github + "\t" + email);
+        System.out.print("9 bottles of beer on the wall, 9 bottles of beer.\nTake one down, pass it around,\n" +
+                "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down, pass it around,\n" +
+                "7 bottles of beer on the wall, 7 bottles of beer.\nTake one down, pass it around,\n" +
+                "6 bottles of beer on the wall, 6 bottles of beer.\nTake one down, pass it around,\n" +
+                "5 bottles of beer on the wall, 5 bottles of beer.\nTake one down, pass it around,\n" +
+                "4 bottles of beer on the wall, 4 bottles of beer.\nTake one down, pass it around,\n" +
+                "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down, pass it around,\n" +
+                "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down, pass it around,\n" +
+                "1 bottles of beer on the wall, 1 bottles of beer.\nTake one down, pass it around,\n" +
+                "Take one down, pass it around,\n" +
+                "No more bottles of beer on the wall.");
+    }
+}
